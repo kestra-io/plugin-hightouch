b23c0ba318cd119a87df83906464820bf06c2712
Antoine Balliet <antoine.balliet@gorgias.com>
2023-08-16 13:34:03 +0200

    implement test for wrong SyncId provided

---
+++ src/test/java/io/kestra/plugin/hightouch/connections/SyncInvalidSyncIdTest.java
@@ -0,0 +1,43 @@
+package io.kestra.plugin.hightouch.connections;
+
+import com.google.common.collect.ImmutableMap;
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.runners.RunContextFactory;
+import io.micronaut.context.annotation.Value;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@MicronautTest
+class SyncInvalidSyncIdTest {
+    @Inject
+    private RunContextFactory runContextFactory;
+
+    @Value("${hightouch.invalid-sync-id}")
+    private Long syncId;
+
+    @Value("${hightouch.token}")
+    private String token;
+
+    @Test
+    void run() throws Exception {
+        RunContext runContext = runContextFactory.of(ImmutableMap.of());
+
+        Sync task = Sync.builder()
+                .token(this.token)
+                .syncId(this.syncId)
+                .build();
+
+        Throwable exception = assertThrows(
+                HttpClientResponseException.class,
+                        () -> task.run(runContext),
+                        "Expected Sync() to throw 404 error with HttpClientResponseException, but it didn't"
+        );
+        assertThat(exception.getMessage(), containsString("Request failed with status '404'"));
+    }
+}
